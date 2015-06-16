eab1dddce828293cd935cedd6a713b1610900b2b
reputet <outofspace230@gmail.com>
2015-06-16 18:39:23 +0300

    >hello world!

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
-$ip=$_SERVER['REMOTE_ADDR'];
-echo "$ip";
+echo "hello world!";
 ?>
-
-<br>
-
-<?php
-echo gethostname();
-?>
-<br>
-<?php
-$browser=$_SERVER['HTTP_USER_AGENT'];
-echo "$browser";
-?>
-<br>
-<?php
-echo date('l jS \of F Y h:i:s A');
-?>
-
